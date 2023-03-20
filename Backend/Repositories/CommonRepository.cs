767a99e04f496c82b334c634ebb7318e81ae5ca8
Anil K Samnani <128317630+anilsamnani@users.noreply.github.com>
2023-03-20 11:51:15 +0530

    starting project
    starting project

---
+++ Backend/Repositories/CommonRepository.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Backend.Repositories
+{
+    public class CommonRepository
+    {
+         protected NpgsqlConnection con;
+        public CommonRepository()
+        {
+        IConfiguration myConfig=new ConfigurationBuilder()
+        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+        .AddJsonFile("appsettings.json")
+        .Build();
+        con=new NpgsqlConnection(myConfig.GetConnectionString("pgconn"));
+        }
+    }
+}
