f5f687118134f8b1290da079eb7049e702b7b5c7
krishnatamakuwala <62591693+krishnatamakuwala@users.noreply.github.com>
2023-03-21 14:31:18 +0530

    Place Api

---
+++ TripAdvisorApi/Models/t_place.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TripAdvisorApi.Models
+{
+    public class t_place
+    {
+        public int c_placeid { get; set; }
+
+        public string c_placename { get; set; }
+
+        public string c_description { get; set; }
+
+        public string c_placeimage { get; set; }
+
+        public string c_preferedmonths { get; set; }
+    }
+}
