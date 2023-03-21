37ab5ccc1787a46a9b90cebfbc639942525afdf9
Jaymin Kania <85449142+jayminkania@users.noreply.github.com>
2023-03-21 15:02:37 +0530

    Rate Api

---
+++ TripAdvisorApi/Controllers/RateController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TripAdvisorApi.Bal;
+using TripAdvisorApi.Models;
+
+namespace TripAdvisorApi.Controllers
+{
+    public class RateController : ApiController
+    {
+        RateHelper rh = new RateHelper();
+        // GET: api/Rate
+ 
+        public List<t_rate> Get()
+        {
+            return rh.GetAll();
+        }
+        // GET: api/Rate/5
+      
+        public t_rate Get(int id)
+        {
+            return rh.GetOne(id);
+        }
+
+        // POST: api/Rate
+        public void Post([FromBody]t_rate value)
+        {
+            rh.Insert(value);
+        }
+
+        // PUT: api/Rate/5
+        public void Put(int id, [FromBody]t_rate value)
+        {
+            rh.Update(value);
+        }
+
+        // DELETE: api/Rate/5
+        public void Delete(int id)
+        {
+            rh.Delete(id);
+        }
+    }
+}
